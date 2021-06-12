3b9eedbcc04d3e3fc46099c09a3262751e758819
Georgi Ditsov <g.ditsov@proxiad.com>
2021-06-12 13:59:42 +0300

    Add the main class of the SB application

---
+++ src/main/java/com/ditsov/school_diary/SchoolDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.ditsov.school_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolDiaryApplication {
+
+    public static void main(String[] args) {
+	SpringApplication.run(SchoolDiaryApplication.class, args);
+    }
+
+}
